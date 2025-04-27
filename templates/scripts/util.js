98ae080b5a972d397845d8b6eface8cb2daa84d0
Vladimir Turov <github.qcw34@simplelogin.com>
2025-04-27 15:59:49 +0300

    Implement adjusting dish total amount

--- templates/scripts/util.js
+++ templates/scripts/util.js
@@ -415,7 +415,7 @@ class HtmlPresets {
 
 
 
-function adjustSpacing() {
+const adjustSpacing = () => {
   document.querySelectorAll('.low-line-height').forEach(el => {
     const style = window.getComputedStyle(el);
     const lineHeight = parseFloat(style.lineHeight);
@@ -445,4 +445,21 @@ function adjustSpacing() {
     }
   });
 }
+
+
+const generateUUID = () => { // Public Domain/MIT
+    let d = new Date().getTime();//Timestamp
+    let d2 = ((typeof performance !== 'undefined') && performance.now && (performance.now()*1000)) || 0;//Time in microseconds since page-load or 0 if unsupported
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        let r = Math.random() * 16;//random number between 0 and 16
+        if(d > 0){//Use timestamp until depleted
+            r = (d + r)%16 | 0;
+            d = Math.floor(d/16);
+        } else {//Use microseconds since page-load if supported
+            r = (d2 + r)%16 | 0;
+            d2 = Math.floor(d2/16);
+        }
+        return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+    });
+};
 
